458c026b9856cd2800ef26937b78414eca17e32e
James Todaro <30529065+jtodaroii@users.noreply.github.com>
2021-07-01 17:08:16 +0000

    added poem.txt and function headers for open_file and write_file

---
+++ main.py
@@ -0,0 +1,12 @@
+def open_file(filename):
+  #add the code to open the file
+  return #the list
+
+def write_file(filename, list_of_words):
+  #add the code to write the file here
+  #dont forget to close your file
+  return
+
+
+list_of_words = open_file("poem.py")
+write_file("poem2.txt", list_of_words)
