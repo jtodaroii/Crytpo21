c8e3cf98204bb1726a35db477019b34da21d1ca6
James Todaro <30529065+jtodaroii@users.noreply.github.com>
2021-07-08 16:01:45 +0000

    add code for all the functions

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 def encrypt_letter(P,K):
   if ord(P)>=ord("a") and ord(P)<=ord("z"):
-    #WRITE THIS
-    encrypted_letter = "?"
+    P = ord(P) - ord("a")
+    C = (P + K) % 26
+    encrypted_letter = chr(C + ord("a"))
     return encrypted_letter
   elif ord(P)>=ord("A") and ord(P)<=ord("Z"):
-    #WRITE THIS
-    encrypted_letter = "?"
+    P = ord(P) - ord("A")
+    C = (P + K) % 26
+    encrypted_letter = chr(C + ord("A"))
     return encrypted_letter
   else:
     return P
@@ -23,15 +25,33 @@ def encrypt_message(P,K):
   return encrypted_message
 
 def decrypt_letter(C,K):
-  #WRITE THIS FUNCTION
-  return C
+  if ord(C)>=ord("a") and ord(C)<=ord("z"):
+    C = ord(C) - ord("a")
+    P = (C - K) % 26
+    decrypted_letter = chr(P + ord("a"))
+    return decrypted_letter
+  elif ord(C)>=ord("A") and ord(C)<=ord("Z"):
+    C = ord(C) - ord("A")
+    P = (C - K) % 26
+    decrypted_letter = chr(P + ord("A"))
+    return decrypted_letter
+  else:
+    return C
 
 def decrypt_word(C,K):
-  #WRITE THIS FUNCTION
-  decrypted_word = ""
+  decrypted_word =""
+  for letter in C:
+    decrypted_word += decrypt_letter(letter,K)
   return decrypted_word
 
 def decrypt_message(C,K):
-  #WRITE THIS FUNCTION
-  decrypted_message =[]
+  decrypted_message=[]
+  for w in C:
+    decrypted_message.append(decrypt_word(w,K))
   return decrypted_message
+
+word = "BEAR"
+e_word = encrypt_word(word, 10)
+print(e_word)
+d_word = decrypt_word(e_word,10)
+print(d_word)
